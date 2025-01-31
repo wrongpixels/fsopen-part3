0c5f13de1c16a5e3862bc1fc25b6730b3b31c88d
Al Moreno <83099648+wrongpixels@users.noreply.github.com>
2025-01-31 17:13:09 +0100

    Exercise 3.8

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = require ("express");
+const express = require("express");
 const morgan = require("morgan");
+
+morgan.token('body', (req) => req.body ? JSON.stringify(req.body) : "")
+const morganFilter = (':method :url :status :res[content-length] - :response-time ms :body')
+
 
 const PORT = 3001;
 let persons = [
@@ -24,29 +28,33 @@ let persons = [
         "number": "39-23-6423122"
     }
 ]
-const getPersonFromID = (id) => persons?persons.find(p => p.id === id):null;
-const getPersonFromName = (name) => persons?persons.find(p => p.name === name):null;
-
+const getPersonFromID = (id) => persons ? persons.find(p => p.id === id) : null;
+const getPersonFromName = (name) => persons ? persons.find(p => p.name === name) : null;
 const getRandomNumber = () => {
     let randomString = "";
     const fixedMin = Math.ceil(0);
     const fixedMax = Math.floor(99999);
-    randomString = String(Math.floor(Math.random()*(fixedMax-fixedMin) + 1 + fixedMin));
+    randomString = String(Math.floor(Math.random() * (fixedMax - fixedMin) + 1 + fixedMin));
     return randomString;
 }
 const getNewRandomID = () => {
     const newID = getRandomNumber();
     const existingPerson = getPersonFromID(newID);
-    if (existingPerson)
-    {
+    if (existingPerson) {
         return getNewRandomID();
     }
     return String(newID);
 }
+const getRequestData = (req) => {
+    if (!req.body || req.body.length === 0) {
+        return "";
+    }
+    return JSON.stringify(req.body);
+}
 
 const app = express();
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan(morganFilter));
 
 app.get("/", (req, res) => {
     res.json({"message": "Phonebook"});
@@ -57,18 +65,16 @@ app.get("/api", (req, res) => {
 })
 
 app.get("/info", (req, res) => {
-    const count = persons?persons.length:0;
+    const count = persons ? persons.length : 0;
     const time = new Date();
     res.send(`Phonebook has info for ${count} people<p>${time}</p>`)
 })
 app.post("/api/persons", (req, res) => {
     const body = req.body;
-    if (!body || !body.name || !body.number)
-    {
+    if (!body || !body.name || !body.number) {
         return res.status(400).json({"Error": "New person is missing name or number"});
     }
-    if (getPersonFromName(body.name))
-    {
+    if (getPersonFromName(body.name)) {
         return res.status(400).json({"Error": `'${body.name}' already exists. Names must be unique.`})
     }
     const newPerson = {
@@ -80,20 +86,16 @@ app.post("/api/persons", (req, res) => {
     res.json(newPerson);
 })
 app.get("/api/persons", (req, res) => {
-    if (persons && persons.length > 0)
-    {
+    if (persons && persons.length > 0) {
         res.json(persons);
-    }
-    else
-    {
+    } else {
         res.status(404).json({"Error": "No people were found."})
     }
 })
 app.get("/api/persons/:id", (req, res) => {
     const id = req.params.id;
     const person = getPersonFromID(id);
-    if (!person)
-    {
+    if (!person) {
         return res.status(404).json({"Error": `Person ${id} was not found`})
     }
     return res.json(person);
@@ -101,17 +103,12 @@ app.get("/api/persons/:id", (req, res) => {
 app.delete("/api/persons/:id", (req, res) => {
     const id = req.params.id;
     const person = getPersonFromID(id);
-    if (person)
-    {
+    if (person) {
         persons = persons.filter(p => p.id !== id);
         res.status(204).end();
-    }
-    else
-    {
+    } else {
         res.status(404).json({"Error": `Person ${id} was not found`});
     }
 })
-
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
